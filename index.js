e32949fc649baa672bf112ed04f2d849d382de10
Graham Bleaney <gbleaney@gmail.com>
2016-01-23 00:48:00 -0500

    Limited scope of plugin to inbox

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var self = require('sdk/self');
 var pageMod = require("sdk/page-mod");
 
 pageMod.PageMod({
-  include: "*",//https://inbox.google.com*",
+  include: "https://inbox.google.com*",
   contentScriptFile: ["./jquery-2.2.0.min.js", "./setOps.js", "./inbox_script.js"]
 });
